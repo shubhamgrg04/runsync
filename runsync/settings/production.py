38ba792c3001331d82ee306f7ed08de13c060ab4
Shubham Garg <shubhamgarg@192.168.1.21>
2025-05-11 12:15:02 +0530

    fix migrations issue

--- runsync/settings/production.py
+++ runsync/settings/production.py
@@ -20,7 +20,7 @@ ALLOWED_HOSTS = ["*"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": f"{BASE_DIR}/db/db.sqlite3",
     }
 }
 
